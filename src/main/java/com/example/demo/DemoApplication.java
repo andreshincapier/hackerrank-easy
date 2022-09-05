6336c006f335a75c57aa3e0db069e02c2d0fbc5f
andreshincapier <andreshincapie82132@gmail.com>
2022-09-05 08:48:22 -0500

    Feature: a very big sum

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,25 +9,13 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        List<Long> prestamo = List.of(15L, 16L, 45L);
-        System.out.println(calcularDias(prestamo));
+        List<Long> prestamo = List.of(1000000001L, 1000000002L, 1000000003L, 1000000004L,
+            1000000005L);
+        System.out.println(aVeryBigSum(prestamo));
     }
 
-    public static List<Long> calcularDias(List<Long> prestamo) {
+    public static Long aVeryBigSum(List<Long> ar) {
 
-        List<Long> result = new ArrayList<>();
-
-        prestamo.forEach(l -> {
-            long accumulator = 1L;
-
-            for (long j = 1; j < l; j++) {
-                accumulator += accumulator;
-                if (accumulator > l) {
-                    result.add(j);
-                    break;
-                }
-            }
-        });
-        return result;
+        return ar.stream().mapToLong(l -> l).sum();
     }
 }
